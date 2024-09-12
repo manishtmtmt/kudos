36664007f9f092ed44347b20bc5efed1ae4c9878
Manish Tiwari <manisht.mt.mt@gmail.com>
2024-09-12 18:05:35 +0530

    implemented authentication, and login routes

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-screen w-full bg-blue-600 font-mono">
         {children}
         <ScrollRestoration />
         <Scripts />
---
+++ app/utils/user.server.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+
+import type { RegisterForm } from "./types.server";
+import { db } from "./prisma.server";
+
+export const createUser = async (user: RegisterForm) => {
+  const passwordHash = await bcrypt.hash(user.password, 10);
+  const newUser = await db.user.create({
+    data: {
+      email: user.email,
+      password: passwordHash,
+      profile: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    },
+  });
+
+  return { id: newUser.id, email: user.email };
+};
